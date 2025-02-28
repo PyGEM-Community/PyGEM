81531bea64ff90b3b0897f535f4a989667fef24c
btobers <tobers.brandon@gmail.com>
2025-02-27 19:22:46 -0500

    config test created

---
+++ pygem/tests/test_config.py
@@ -0,0 +1,24 @@
+"""
+Python Glacier Evolution Model (PyGEM)
+
+copyright © 2018 David Rounce <drounce@cmu.edu>
+
+Distrubted under the MIT lisence
+"""
+from pygem.setup.config import ConfigManager
+
+def test_update_config():
+    updates = {
+        "sim.nsims": "5",
+        "user.email": "updated@example.com",
+        "constants.density_ice": "850"
+    }
+    
+    config_manager = ConfigManager()
+    config_manager.ensure_config(overwrite=True)
+    config_manager.update_config(updates)
+    config = config_manager.read_config()
+    
+    assert config["sim"]["nsims"] == 5
+    assert config["user"]["email"] == "updated@example.com"
+    assert config["constants"]["density_ice"] == 850
