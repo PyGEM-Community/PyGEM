1a98716824cb5ecf3d0e845a61ca618e3cc37d31
btobers <tobers.brandon@gmail.com>
2025-05-06 10:52:46 -0400

    specify order of test notebooks

--- pygem/tests/test_notebooks.py
+++ pygem/tests/test_notebooks.py
@@ -7,17 +7,23 @@ import pytest
 nb_dir = os.environ.get('PYGEM_NOTEBOOKS_DIRPATH') or os.path.join(
     os.path.expanduser('~'), 'PyGEM-notebooks'
 )
-notebooks = [f for f in os.listdir(nb_dir) if f.endswith('.ipynb')]
+# TODO #54: Test all notebooks
+# notebooks = [f for f in os.listdir(nb_dir) if f.endswith('.ipynb')]
+
+# list of notebooks to test, in the desired order
+notebooks = [
+    'simple_test.ipynb',
+    'advanced_test.ipynb',
+    'advanced_test_tw.ipynb',
+]
 
 
 @pytest.mark.parametrize('notebook', notebooks)
 def test_notebook(notebook):
-    # TODO #54: Test all notebooks
+    """
+    Run pytest with nbmake on the specified notebook.
 
-    if notebook not in (
-        'simple_test.ipynb',
-        'advanced_test.ipynb',
-        'advanced_test_tw.ipynb',
-    ):
-        pytest.skip()
+    This test is parameterized to run each notebook individually,
+    preserving the order defined in the `notebooks` list.
+    """
     subprocess.check_call(['pytest', '--nbmake', os.path.join(nb_dir, notebook)])
