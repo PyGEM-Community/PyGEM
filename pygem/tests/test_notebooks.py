d98775603a5b8e4bd7c9edd70f2527ee926a9c91
Brandon S. Tober <tobers.brandon@gmail.com>
2025-05-28 13:04:56 -0400

    Revert "specify order of test notebooks"
    This reverts commit 67e786e460b2a6140c8e4f4d69880bf17abdeec4.

--- pygem/tests/test_notebooks.py
+++ pygem/tests/test_notebooks.py
@@ -7,23 +7,17 @@ import pytest
 nb_dir = os.environ.get('PYGEM_NOTEBOOKS_DIRPATH') or os.path.join(
     os.path.expanduser('~'), 'PyGEM-notebooks'
 )
-# TODO #54: Test all notebooks
-# notebooks = [f for f in os.listdir(nb_dir) if f.endswith('.ipynb')]
-
-# list of notebooks to test, in the desired order
-notebooks = [
-    'simple_test.ipynb',
-    'advanced_test.ipynb',
-    'advanced_test_tw.ipynb',
-]
+notebooks = [f for f in os.listdir(nb_dir) if f.endswith('.ipynb')]
 
 
 @pytest.mark.parametrize('notebook', notebooks)
 def test_notebook(notebook):
-    """
-    Run pytest with nbmake on the specified notebook.
+    # TODO #54: Test all notebooks
 
-    This test is parameterized to run each notebook individually,
-    preserving the order defined in the `notebooks` list.
-    """
+    if notebook not in (
+        'simple_test.ipynb',
+        'advanced_test.ipynb',
+        'advanced_test_tw.ipynb',
+    ):
+        pytest.skip()
     subprocess.check_call(['pytest', '--nbmake', os.path.join(nb_dir, notebook)])
