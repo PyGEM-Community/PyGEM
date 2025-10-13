Bug fix in density scaling if -inf model output returned for bad step

pygem/bin/run/run_calibration.py
@@ -327,7 +327,7 @@ def calculate_elev_change_1d(
     # if there is an issue evaluating the dynamics model for a given parameter set in MCMC calibration,
     # return -inf for mb_mwea and binned_dh, so MCMC calibration won't accept given parameters
     except RuntimeError:
-        return -np.inf, -np.inf
+        return float('-inf'), float('-inf')
 
     ### get monthly ice thickness
     # grab components of interest
@@ -2196,8 +2196,9 @@ def run(list_packed_vars):
                                     )
                                     if i == 1:
                                         graphics.plot_mcmc_elev_change_1d(
-                                            gdir.elev_change_1d,
                                             pred_chain[1],
+                                            fls,
+                                            gdir.elev_change_1d,
                                             gdir.ela.min(),
                                             glacier_str,
                                             show=show,

pygem/mcmc.py
@@ -226,6 +226,11 @@ class mbPosterior:
     def log_likelihood(self, m):
         log_likehood = 0
         for i, pred in enumerate(self.preds):
+            # --- Check for invalid predictions early ---
+            if torch.all(pred == float('-inf')):
+                # Invalid model output -> assign -inf likelihood
+                return torch.tensor([-float('inf')])
+        
             if i == 0:
                 # --- Base case: mass balance likelihood ---
                 log_likehood += log_normal_density(
@@ -233,8 +238,9 @@ class mbPosterior:
                     mu=pred,  # predicted values
                     sigma=self.obs[i][1],  # observation uncertainty
                 )
+            
             elif i == 1 and len(m) > 3:
-                # --- Extended case: apply density scaling to get binned mass change ---
+                # --- Extended case: apply density scaling to get binned elevation change ---
                 # Create density field, separate values for ablation/accumulation zones
                 rho = np.ones_like(self.bin_z)
                 rho[self.abl_mask] = m[3]  # rhoabl
@@ -245,9 +251,9 @@ class mbPosterior:
                 )  # scale prediction by model density values (convert from m ice to m thickness change)
 
                 log_likehood += log_normal_density(
-                    self.obs[i][0],  # observations
-                    mu=pred,  # scaled predictions
-                    sigma=self.obs[i][1],  # uncertainty
+                    self.obs[i][0],         # observations
+                    mu=pred,                # scaled predictions
+                    sigma=self.obs[i][1],   # uncertainty
                 )
         return log_likehood
 