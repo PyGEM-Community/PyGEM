b1f89f7ab23ebd111e098021cb9b27b379af3621
brandon s. tober <tobers.brandon@gmail.com>
2025-10-22 12:52:25 -0400

    Export regional Glen A from calibrated inversion

--- pygem/bin/run/run_inversion.py
+++ pygem/bin/run/run_inversion.py
@@ -6,6 +6,8 @@ from functools import partial
 import numpy as np
 import pandas as pd
 
+pd.set_option('display.float_format', '{:.3e}'.format)
+
 # pygem imports
 from pygem.setup.config import ConfigManager
 
@@ -13,7 +15,8 @@ from pygem.setup.config import ConfigManager
 config_manager = ConfigManager()
 # read the config
 pygem_prms = config_manager.read_config()
-from oggm import cfg, tasks, workflow
+from oggm import cfg, tasks, utils, workflow
+from oggm.exceptions import InvalidWorkflowError
 
 import pygem.pygem_modelsetup as modelsetup
 from pygem import class_climate
@@ -24,11 +27,89 @@ from pygem.oggm_compat import update_cfg
 from pygem.shop import debris, mbdata
 from pygem.utils._funcs import str2bool
 
-cfg.initialize()
+cfg.initialize(logging_level=pygem_prms['oggm']['logging_level'])
 cfg.PATHS['working_dir'] = f'{pygem_prms["root"]}/{pygem_prms["oggm"]["oggm_gdir_relpath"]}'
 
 
-def run(glac_no, ncores=1, calibrate_regional_glen_a=False, glen_a=None, fs=None, reset_gdirs=False, debug=False):
+def export_regional_results(regions, outpath):
+    # Directory containing the per-region CSVs
+    outdir, outname = os.path.split(outpath)
+    # loop through regional output dataframes
+    dfs = []
+    filepaths_to_delete = []
+    for r in regions:
+        # construct the filename using zero-padded format
+        filename = outname.replace('.csv', f'_R{str(r).zfill(2)}.csv')
+        filepath = os.path.join(outdir, filename)
+        if os.path.exists(filepath):
+            df = pd.read_csv(filepath)
+            df['rnum'] = r  # for sorting later
+            dfs.append(df)
+            filepaths_to_delete.append(filepath)
+        else:
+            print(f'Warning: {filepath} not found')
+
+    # merge all into one DataFrame
+    merged_df = pd.concat(dfs, ignore_index=True)
+
+    # sort by the region number
+    merged_df = merged_df.sort_values('rnum').drop(columns='rnum')
+
+    # if the file already exists, replace rows with same '01Region'
+    if os.path.exists(outpath):
+        existing_df = pd.read_csv(outpath)
+        # remove rows with the same '01Region' values as in the new merge
+        merged_df = pd.concat(
+            [existing_df[~existing_df['01Region'].isin(merged_df['01Region'])], merged_df], ignore_index=True
+        )
+        # re-sort
+        merged_df = merged_df.sort_values('01Region')
+
+    # export final merged csv
+    merged_df.to_csv(outpath, index=False)
+
+    # Delete individual regional files
+    for fp in filepaths_to_delete:
+        os.remove(fp)
+
+
+def get_regional_volume(gdirs, ignore_missing=True):
+    """
+    Calculate the modeled volume [m3] and consensus volume [m3] for the given set of glaciers
+    """
+    # get itmix vol
+    # Get the ref data for the glaciers we have
+    df = pd.read_hdf(utils.get_demo_file('rgi62_itmix_df.h5'))
+    rids = [gdir.rgi_id for gdir in gdirs]
+
+    found_ids = df.index.intersection(rids)
+    if not ignore_missing and (len(found_ids) != len(rids)):
+        raise InvalidWorkflowError(
+            'Could not find matching indices in the '
+            'consensus estimate for all provided '
+            'glaciers. Set ignore_missing=True to '
+            'ignore this error.'
+        )
+
+    df = df.reindex(rids)
+    itmix_vol = df.sum()['vol_itmix_m3']
+    model_vol = 0
+    for gdir in gdirs:
+        model_vol += gdir.read_pickle('model_flowlines')[0].volume_m3
+    return itmix_vol, model_vol
+
+
+def run(
+    glac_no,
+    ncores=1,
+    calibrate_regional_glen_a=False,
+    glen_a=None,
+    fs=None,
+    reset_gdirs=False,
+    regional_inv=False,
+    outpath=None,
+    debug=False,
+):
     """
     Run OGGM's bed inversion for a list of RGI glacier IDs using PyGEM's mass balance model.
     """
@@ -137,6 +218,7 @@ def run(glac_no, ncores=1, calibrate_regional_glen_a=False, glen_a=None, fs=None
     ### CALCULATE APPARENT MASS BALANCE ###
     #######################################
     # note, PyGEMMassBalance_wrapper is passed to `tasks.apparent_mb_from_any_mb` as the `mb_model_class` so that PyGEMs mb model is used for apparent mb
+    # apply inversion_filter on mass balance with debris to avoid negative flux
     workflow.execute_entity_task(
         tasks.apparent_mb_from_any_mb,
         gdirs,
@@ -144,6 +226,7 @@ def run(glac_no, ncores=1, calibrate_regional_glen_a=False, glen_a=None, fs=None
             PyGEMMassBalance_wrapper,
             fl_str='inversion_flowlines',
             option_areaconstant=True,
+            inversion_filter=pygem_prms['mb']['include_debris'],
         ),
     )
     # add debris data to flowlines
@@ -157,7 +240,7 @@ def run(glac_no, ncores=1, calibrate_regional_glen_a=False, glen_a=None, fs=None
     if calibrate_regional_glen_a:
         if debug:
             print("Calibrating Glen's A")
-        workflow.calibrate_inversion_from_consensus(
+        cdf = workflow.calibrate_inversion_from_consensus(
             gdirs,
             apply_fs_on_mismatch=True,
             error_on_mismatch=False,  # if you running many glaciers some might not work
@@ -165,6 +248,8 @@ def run(glac_no, ncores=1, calibrate_regional_glen_a=False, glen_a=None, fs=None
             # the equilibrium assumption for retreating glaciers (see. Figure 5 of Maussion et al. 2019)
             volume_m3_reference=None,  # here you could provide your own total volume estimate in m3
         )
+        itmix_vol = cdf.sum()['vol_itmix_m3']
+        model_vol = cdf.sum()['vol_oggm_m3']
 
     for gdir in gdirs:
         if calibrate_regional_glen_a:
@@ -258,11 +343,40 @@ def run(glac_no, ncores=1, calibrate_regional_glen_a=False, glen_a=None, fs=None
     # add debris to model_flowlines
     workflow.execute_entity_task(debris.debris_binned, gdirs, fl_str='model_flowlines')
 
+    # get itmix and inversion cumulative volumes
+    if not calibrate_regional_glen_a:
+        itmix_vol, model_vol = get_regional_volume(gdirs)
+
+    reg = glac_no[0].split('.')[0].zfill(2)
+    # prepare ouptut dataset
+    df = pd.Series(
+        {
+            '01Region': reg,
+            'count': len(glac_no),
+            'inversion_glen_a': gdirs[0].get_diagnostics()['inversion_glen_a'],
+            'inversion_fs': gdirs[0].get_diagnostics()['inversion_fs'],
+            'vol_itmix_m3': itmix_vol,
+            'vol_model_m3': model_vol,
+        }
+    )
+
+    if debug:
+        print(df)
+
+    # export
+    if outpath:
+        if calibrate_regional_glen_a and regional_inv:
+            pd.DataFrame([df]).to_csv(outpath.replace('.csv', f'_R{reg}.csv'), index=False)
+        else:
+            raise ValueError(
+                'Only set up to export regional Glen A parameters if regionally calibrated against the regional ice volume estimate.'
+            )
+
 
 def main():
     # define ArgumentParser
     parser = argparse.ArgumentParser(
-        description="Perform glacier bed inversion (defaults to find best Glen's A for each RGI order 01 region)"
+        description="Perform glacier bed inversion (defaults to finding best Glen's A for each RGI order 01 region)"
     )
     # add arguments
     parser.add_argument(
@@ -274,7 +388,6 @@ def main():
     )
     parser.add_argument(
         '-rgi_glac_number',
-        action='store',
         type=float,
         default=pygem_prms['setup']['glac_no'],
         nargs='+',
@@ -283,7 +396,6 @@ def main():
     (
         parser.add_argument(
             '-rgi_glac_number_fn',
-            action='store',
             type=str,
             default=None,
             help='Filepath containing list of rgi_glac_number, helpful for running batches on spc',
@@ -309,10 +421,15 @@ def main():
     )
     parser.add_argument(
         '-ncores',
-        action='store',
         type=int,
         default=1,
         help='Number of simultaneous processes (cores) to use',
+    )
+    parser.add_argument(
+        '-outpath',
+        type=str,
+        default=f'{pygem_prms["root"]}/{pygem_prms["sim"]["oggm_dynamics"]["glen_a_regional_relpath"]}',
+        help='Output datapath',
     )
     parser.add_argument(
         '-reset_gdirs',
@@ -340,6 +457,7 @@ def main():
             )['rgino_str'].values.tolist()
             for r01 in args.rgi_region01
         ]
+        regional_inv = True  # flag to regional inversion
     else:
         batches = None
         if args.rgi_glac_number:
@@ -347,6 +465,7 @@ def main():
             # format appropriately
             glac_no = [float(g) for g in glac_no]
             batches = [f'{g:.5f}' if g >= 10 else f'0{g:.5f}' for g in glac_no]
+            regional_inv = False  # flag to indicate per-glacier inversion
         elif args.rgi_glac_number_fn is not None:
             with open(args.rgi_glac_number_fn, 'r') as f:
                 batches = json.load(f)
@@ -359,11 +478,16 @@ def main():
         glen_a=args.glen_a,
         fs=args.fs,
         reset_gdirs=args.reset_gdirs,
+        regional_inv=regional_inv,
+        outpath=args.outpath,
         debug=args.debug,
     )
 
     for i, batch in enumerate(batches):
         run_partial(batch)
+
+    if args.outpath and args.calibrate_regional_glen_a:
+        export_regional_results(args.rgi_region01, args.outpath)
 
 
 if __name__ == '__main__':
